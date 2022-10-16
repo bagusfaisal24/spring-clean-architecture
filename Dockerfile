3b6fe0001d8fda19b8c3ed4809c5b524c0b62b52
bagusfaisal24 <bagus.khafidz@gmail.com>
2022-10-16 09:54:19 +0700

    test

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:8-jdk-alpine
+ARG JAR_FILE=target/*.jar
+WORKDIR /opt
+ENV PORT 8081
+EXPOSE 8081
+COPY ${JAR_FILE} app-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","architecture-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/clean/architecture/controller/ProductController.java
+++ src/main/java/com/clean/architecture/controller/ProductController.java
@@ -1,9 +1,22 @@
 package com.clean.architecture.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/v1/product")
 public class ProductController {
+
+    @GetMapping("server-time")
+    public Map<String, Object> getServerTime(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("time", LocalDateTime.now());
+        return map;
+    }
 }
